bf010f4b570ceb027cce3888faf37c1abb8aeb6b
BrLocha94 <brunolocha2@gmail.com>
2020-02-01 16:53:57 -0300

    Created instance prefab

---
+++ LifeIsAShip/Assets/Scripts/__instancesScripts/SceneController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneController : MonoBehaviour
+{
+    private static SceneController _instance;
+    public static SceneController instance()
+    {
+        if (_instance != null)
+            return _instance;
+
+        _instance = GameObject.FindObjectOfType<SceneController>();
+
+        if(_instance == null)
+        {
+            GameObject resourceObject = Resources.Load("SceneController") as GameObject;
+            if(resourceObject != null)
+            {
+                GameObject instantiatedObject = Instantiate(resourceObject);
+                _instance = instantiatedObject.GetComponent<SceneController>();
+                DontDestroyOnLoad(instantiatedObject);
+            }
+        }
+
+        return _instance;
+    }
+}
