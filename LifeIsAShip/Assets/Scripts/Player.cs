ccb3dfe3ca58b2eff0b5d7ee4955ba998f13a1b8
BrLocha94 <brunolocha2@gmail.com>
2020-02-01 15:47:40 -0300

    Updated project base. Cretaed base controller for player and script

---
+++ LifeIsAShip/Assets/Scripts/Player.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+public class Player : MonoBehaviour
+{
+    [SerializeField]
+    private Animator animator;
+
+    float animCooldownTimer = 1f;
+    float currentTime = 1f;
+
+	void Update ()
+    {
+        CheckBasicInput();
+    }
+
+    private void CheckBasicInput()
+    {
+        if (currentTime >= animCooldownTimer)
+        {
+            if (Input.GetKeyDown(KeyCode.W))
+            {
+                animator.Play("up");
+                currentTime = 0f;
+            }
+            else if (Input.GetKeyDown(KeyCode.S))
+            {
+                animator.Play("down");
+                currentTime = 0f;
+            }
+            else if (Input.GetKeyDown(KeyCode.A))
+            {
+                animator.Play("left");
+                currentTime = 0f;
+            }
+            else if (Input.GetKeyDown(KeyCode.D))
+            {
+                animator.Play("right");
+                currentTime = 0f;
+            }
+        }
+        else
+            currentTime += Time.deltaTime;
+    }
+}
