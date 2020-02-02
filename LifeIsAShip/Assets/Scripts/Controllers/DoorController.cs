26f7ff0d8a6b90bd89054298b4854d430398e39d
Erick Guimaraes <erickgdeoliveira@gmail.com>
2020-02-02 19:03:23 -0300

    Adding door controller and initial behaviour conifguration

---
+++ LifeIsAShip/Assets/Scripts/Controllers/DoorController.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorController : MonoBehaviour {
+
+
+    [Header("Door from Game")]
+    [SerializeField]
+    private GameObject[] Doors;
+
+}
