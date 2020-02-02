ed0dd335c163d4a99a6227dd5179f003710adc0a
joaohanson <joaohanson.gd@gmail.com>
2020-02-02 16:31:48 -0300

    Revert "Revert "Revert "Revert "Merge branch 'master' of https://github.com/BrLocha94/GlobalGameJam2020""""
    This reverts commit d5b54780b073c4d9bba5aabe86320c5914763ecc.

---
+++ LifeIsAShip/Assets/Scripts/MinigameRelease.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MinigameRelease : Minigame
+{
+    bool isOver = false;
+    bool checking = false;
+
+    void Update()
+    {
+        if (checking == false)
+        {
+            if(isOver == true && Input.GetMouseButtonDown(0))
+                checking = true;
+        }
+        else
+        {
+            if(isOver == true && !Input.GetMouseButton(0))
+            {
+                //Released
+                //Check if win or not
+                // call OnFinishedGame(bool result) passing the check resutl
+            }
+            else if(isOver == false)
+            {
+                //Is not over the object
+                checking = false;
+            }
+            else
+            {
+                //Fill behaviour
+            }
+        }
+    }
+
+    private void OnMouseEnter()
+    {
+        isOver = true;
+    }
+
+    private void OnMouseExit()
+    {
+        isOver = false;
+    }
+}
---
+++ LifeIsAShip/Assets/Scripts/NodeConfig.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NodeConfig : MonoBehaviour
+{
+    public Minigame miniGame;
+    public RuntimeAnimatorController animatorController;
+
+}
