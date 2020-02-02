9361a84cfc73bd0b7bae42a25efabbb8c63770ff
BrLocha94 <brunolocha2@gmail.com>
2020-02-02 03:45:35 -0300

    Animation updated

---
+++ LifeIsAShip/Assets/Scripts/Controllers/CutsceneController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CutsceneController : MonoBehaviour
+{
+    [Header("Scene animators")]
+    public Animator rickGuyAnimator;
+    public Animator poorGuyAnimator;
+
+
+    bool used = false;
+
+    void Update()
+    {
+        if (used == false)
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+                AdvanceAnimation();
+        }    
+    }
+
+    public void AdvanceAnimation()
+    {
+        rickGuyAnimator.SetTrigger("go");
+        used = true;
+    }
+}
---
+++ LifeIsAShip/Assets/Scripts/MessageHandler.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MessageHandler : MonoBehaviour
+{
+    [Header("Hammer prefab and position to instantiate")]
+    public Hammer hammer;
+    public Transform position;
+
+	public void OnHandAnimationExit()
+    {
+        GameObject newHammer = Instantiate(hammer.gameObject, position);
+        newHammer.transform.SetParent(gameObject.transform);
+    }
+}
